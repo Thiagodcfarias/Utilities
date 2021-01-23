a5750af32da0b8c61a1129faca7179880131c5fc
Thiagodcfarias <thiagodcfarias@live.com>
2021-01-23 09:29:10 -0300

    Create download_video.py

---
+++ file manipulation/pytube/download_video.py
@@ -0,0 +1,12 @@
+import pytube as p
+
+# edit this variables
+url = 'https://www.youtube.com/watch?v=xd7Lg2CtvTM'
+path = 'C:\\Users\\samsung\\Desktop'
+nome_arquivo = 'teste'
+
+youtube = p.YouTube(url)
+
+video = youtube.streams.get_highest_resolution()
+video.download(output_path = path, filename = nome_arquivo)
+
