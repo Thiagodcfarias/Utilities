b0f7ded105f070b26c2e08bbb8897e0ca1b33315
Thiago Farias <thiagodcfarias@live.com>
2021-08-28 09:48:30 -0300

    Update download_music.py

--- file manipulation/pytube/download_music.py
+++ file manipulation/pytube/download_music.py
@@ -1,8 +1,13 @@
 import pytube
 
-url = 'https://www.youtube.com/watch?v=C01ctNg0y5E'
+url = 'https://www.youtube.com/watch?v=0JJvSNWfpBc'
 path = 'C:\\Users\\Casa\\Desktop'
 
 musica = pytube.YouTube(url).streams.filter(only_audio=True).first()
-nome_musica = musica.title.replace('|','') # be careful with special characters in music´s name, it´s rainsing OSError [Errno 22] 
+nome_musica = musica.title
+
+# be careful with special characters in music´s name, it´s rainsing OSError [Errno 22] 
+for special_char in ['|',':']:
+    nome_musica = nome_musica.replace(special_char,'') 
+    
 musica.download(output_path = path, filename = nome_musica + '.mp3')
