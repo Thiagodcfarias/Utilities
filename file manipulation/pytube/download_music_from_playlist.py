46e83adff1afe55a506c83465bc3806647770e09
Thiago Farias <thiagodcfarias@live.com>
2021-08-28 09:31:16 -0300

    Update download_music_from_playlist.py

--- file manipulation/pytube/download_music_from_playlist.py
+++ file manipulation/pytube/download_music_from_playlist.py
@@ -1,25 +1,11 @@
 import pytube as p
-from moviepy.editor import VideoFileClip
 
 # edit this variables
-url = 'https://www.youtube.com/playlist?list=PLt8FmCIXMl280bnpT4VAotkVkcN33R8qL'
-path = 'C:\\Users\\Casa\\.spyder-py3\\'
+url = 'https://www.youtube.com/watch?v=RsUD8CTDdAw&list=PLyRcl7Q37-DUM5Fx1DqYEfxfnjNvNfnPL'
+path = 'C:\\Users\\Casa\\Desktop'
 
-playlist = p.Playlist(url)
+for url in playlist:
+    musica = pytube.YouTube(url).streams.filter(only_audio=True).first()
+    nome_musica = musica.title.replace('|','') # be careful with special characters in music´s name, it´s rainsing OSError [Errno 22] 
+    musica.download(output_path = path, filename = nome_musica + '.mp3')
 
-for video in playlist.videos:
-    video = video.streams.get_highest_resolution()
-    video.download()
-    
-    mp4_file = path + video.title + '.mp4'
-    mp3_file = path + video.title + '.mp3'
-    
-    print(mp4_file)
-    
-    videoclip = VideoFileClip(mp4_file)
-    audioclip = videoclip.audio
-    audioclip.write_audiofile(mp3_file)
-    audioclip.close()
-    videoclip.close()
-
-print('============== Done 100% cabou ================')
